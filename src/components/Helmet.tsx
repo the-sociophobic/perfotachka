7896c83cfbe638602cefbdedb2d717d1910a88fd
the_sociophobic <torikowash@gmail.com>
2021-09-09 07:01:12 +0300

    fix og-props

--- src/components/Helmet.tsx
+++ src/components/Helmet.tsx
@@ -22,10 +22,6 @@ class Helmet extends React.Component<RouteComponentProps<PathParamsType>> {
         <title>
           Фестиваль Перфотачки
         </title>
-        <meta property="og:url" content={`https://perfobufet.ru`} />
-        <meta property="og:title" content='Фестиваль Перфотачки' />
-        <meta property="og:description" content='Самоорганизованной фестиваль, программа которого полностью собрана вокруг Перфотачки — серой Toyota Corolla 1987 года.' />
-        <meta property="og:image" content={`https://sun9-53.userapi.com/impg/fgvSQrLlQ991adYtc4Z1R6Ho1C9NjzVYOMVmxQ/O6v8LRrnH78.jpg?size=1200x630&quality=96&sign=d542cf953592ff697e1f3569b458f8fd&type=album`} />
       </ReactHelmet>
     )
   }
