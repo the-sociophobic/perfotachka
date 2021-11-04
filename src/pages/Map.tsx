e77b9f1f2ee73c28768f01581e2d5f986252cc59
the-sociophobic <torikowashi@gmail.com>
2021-11-05 01:37:07 +0300

    fix mapRef, change ifram to object to fix lazy loading bugs

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -9,15 +9,15 @@ class Map extends React.Component {
 
   render = () =>
     <div
-      ref={this.context.MapRef}
+      ref={this.context.mapRef}
       className='container'
     >
       <h1 className='h1 h1--xl font-orange'>
         Локации Перфотачки
       </h1>
       <div className='box p-4 mb-s mb-md-m'>
-      <iframe
-        src="https://www.google.com/maps/d/u/1/embed?mid=1Z0iivN38Gc3fuzw5Ww0KcgwVLXShIX4A"
+      <object
+        data="https://www.google.com/maps/d/u/1/embed?mid=1Z0iivN38Gc3fuzw5Ww0KcgwVLXShIX4A&zoom=12"
         width="100%"
         height="480"
       />
